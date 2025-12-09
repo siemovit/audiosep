880c5f12b56cdd8073cdb552e628a344b512e0b7
Simon Sassi <12765875+maxerbox@users.noreply.github.com>
2025-12-09 11:05:49 +0100

    chore: add wandb dataset upload and download scripts

--- scripts/upload_dataset_wandb.py
+++ scripts/upload_dataset_wandb.py
@@ -1,10 +1,10 @@
 import wandb
+
 run = wandb.init(project="audiosep", job_type="upload")
-# path to my local dataset
-dir = "file:///Users/yannis/audiosep/data"
+# path to my remote data directory in Google Cloud Storage
 # create a regular artifact
-dataset_at = wandb.Artifact('data',type="raw_data")
+dataset_at = wandb.Artifact("data", type="raw_data")
 # creates a checksum for each file and adds a reference to the bucket
 # instead of uploading all of the contents
-dataset_at.add_reference(dir)
+dataset_at.add_dir("data")
 run.log_artifact(dataset_at)
